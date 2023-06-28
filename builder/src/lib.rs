efd63df92c6ba3abf4d01adcbe86df46627b7329
VirusCamp <viruscamp@gmail.com>
2023-06-28 15:35:05 +0800

    builder/tests/01-parse.rs use syn to parse input return empty TokenStream

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::*;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let derive_input = parse_macro_input!(input as DeriveInput);
+    dbg!(&derive_input);
+    TokenStream::new()
 }

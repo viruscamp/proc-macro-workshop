cd9b44f0ebf3a73ef9abb5d020b66d66c37b8aa3
VirusCamp <viruscamp@gmail.com>
2023-06-28 15:48:34 +0800

    builder/tests/03-call-setters.rs setters without chain-call

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,20 +6,46 @@ use quote::*;
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let struct_name = input.ident;
-    let struct_builder_name = format_ident!("{struct_name}Builder");
+    if let Data::Struct(DataStruct { fields: Fields::Named(ref fields), .. }) = input.data {
 
-    let expanded = quote! {
-        impl #struct_name {
-            pub fn builder() -> #struct_builder_name {
-                #struct_builder_name::default()
+        let struct_name = input.ident;
+        let struct_builder_name = format_ident!("{struct_name}Builder");
+
+        let mut fields_builder = vec![];
+        let mut methods_builder = vec![];
+
+        for f in &fields.named {
+            let Field { ident, ty, ..} = f;
+            fields_builder.push(quote! {
+                #ident: Option<#ty>
+            });
+            methods_builder.push(quote! {
+                pub fn #ident(&mut self, v: #ty){
+                    self.#ident = Some(v);
+                }
+            });
+        }
+    
+        let expanded = quote! {
+            impl #struct_name {
+                pub fn builder() -> #struct_builder_name {
+                    <#struct_builder_name as Default>::default()
+                }
             }
-        }
-
-        #[derive(Default)]
-        pub struct #struct_builder_name {
-        }
-    };
-
-    TokenStream::from(expanded)
+    
+            #[derive(Default)]
+            pub struct #struct_builder_name {
+                #(#fields_builder),*
+            }
+    
+            impl #struct_builder_name {
+                #(#methods_builder)*
+            }
+        };
+    
+        TokenStream::from(expanded)
+    } else {
+        // TODO error
+        TokenStream::new()
+    }
 }

64284e4c3b5ec20c6dadcaae139d5ccf5c92d9ac
VirusCamp <viruscamp@gmail.com>
2023-06-28 15:41:17 +0800

    builder/tests/02-create-builder.rs use quote to generate output

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,25 @@
 use proc_macro::TokenStream;
 use syn::*;
+use quote::*;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let derive_input = parse_macro_input!(input as DeriveInput);
-    dbg!(&derive_input);
-    TokenStream::new()
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let struct_name = input.ident;
+    let struct_builder_name = format_ident!("{struct_name}Builder");
+
+    let expanded = quote! {
+        impl #struct_name {
+            pub fn builder() -> #struct_builder_name {
+                #struct_builder_name::default()
+            }
+        }
+
+        #[derive(Default)]
+        pub struct #struct_builder_name {
+        }
+    };
+
+    TokenStream::from(expanded)
 }

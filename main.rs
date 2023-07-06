046c8951da424331f9d6b1fd198d5c199c310a96
VirusCamp <viruscamp@gmail.com>
2023-07-06 16:27:54 +0800

    seq/tests/09-interaction-with-macrorules.rs

--- main.rs
+++ main.rs
@@ -379,3 +379,43 @@ fn seq8() {
         }
     });
 }
+
+fn seq9() {
+        use seq::seq;
+
+    // Source of truth. Call a given macro passing nproc as argument.
+    //
+    // We want this number to appear in only one place so that updating this one
+    // number will correctly affect anything that depends on the number of procs.
+    macro_rules! pass_nproc {
+        ($mac:ident) => {
+            $mac! { 256 }
+        };
+    }
+
+    macro_rules! literal_identity_macro {
+        ($nproc:literal) => {
+            $nproc
+        };
+    }
+
+    // Expands to: `const NPROC: usize = 256;`
+    const NPROC: usize = pass_nproc!(literal_identity_macro);
+
+    struct Proc;
+
+    impl Proc {
+        const fn new() -> Self {
+            Proc
+        }
+    }
+
+    macro_rules! make_procs_array {
+        ($nproc:literal) => {
+            seq!(N in 0..$nproc { [#(Proc::new(),)*] })
+        }
+    }
+
+    // Expands to: `static PROCS: [Proc; NPROC] = [Proc::new(), ..., Proc::new()];`
+    static PROCS: [Proc; NPROC] = pass_nproc!(make_procs_array);
+}
